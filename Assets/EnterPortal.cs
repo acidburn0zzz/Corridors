287dd7a5febf5933e24324a46eb4d0abf8a812e8
Andre Behrens <andre.behrens@haw-hamburg.de>
2014-06-23 00:44:33 +0200

    Added sounds in game

--- Assets/EnterPortal.cs
+++ Assets/EnterPortal.cs
@@ -24,9 +24,28 @@ public class EnterPortal : MonoBehaviour {
 				playerObject.transform.position = targetPlane.transform.position;
 				playerObject.transform.eulerAngles = new Vector3(0, 0, 0);
 				//collision.transform.position = new Vector3(-4.02239f, -0.7922654f, 0.04430723f);
+				SetAudio(playerObject, target);
 			}else{
 				print(target + "not found.");
 			}
+		}
+	}
+
+	private void SetAudio(GameObject playerObject, string targetCorridor)
+	{
+		Jukebox theJukebox = (Jukebox) playerObject.GetComponent(typeof(Jukebox));
+		if (targetCorridor == "planeGang1"){
+			theJukebox.SwitchMusic (Jukebox.Sound4Corridor.Gang1);
+		}else if (targetCorridor == "planeGang2"){
+			theJukebox.SwitchMusic (Jukebox.Sound4Corridor.Gang2);
+		}else if (targetCorridor == "planeGang3"){
+			theJukebox.SwitchMusic (Jukebox.Sound4Corridor.Gang3);
+		}else if (targetCorridor == "planeGang4"){
+			theJukebox.SwitchMusic (Jukebox.Sound4Corridor.Gang4);
+		}else if (targetCorridor == "planeGang5"){
+			theJukebox.SwitchMusic (Jukebox.Sound4Corridor.Gang5);
+		}else{
+			print ("Wrong arguments for changing the sound...");
 		}
 	}
 }
--- Assets/Scripts/Jukebox.cs
+++ Assets/Scripts/Jukebox.cs
@@ -1,15 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Jukebox : MonoBehaviour {
 
+	public List<AudioClip> myList = new List<AudioClip>();
+
+	public enum Sound4Corridor
+	{
+		Gang1,
+		Gang2,
+		Gang3,
+		Gang4,
+		Gang5
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		myList.Add (Resources.Load("Music/Stimmung 01") as AudioClip);
+		myList.Add (Resources.Load("Music/Stimmung 02") as AudioClip);
+		myList.Add (Resources.Load("Music/Stimmung 03") as AudioClip);
+		myList.Add (Resources.Load("Music/Stimmung 04") as AudioClip);
+		myList.Add (Resources.Load("Music/Stimmung 05") as AudioClip);
+
+		// Set Music at beginning
+		SwitchMusic (Sound4Corridor.Gang1);
+	}
+
+	public void SwitchMusic(Sound4Corridor corridor)
+	{
+		AudioClip clipToPlay = null;
+		print ("Switching to " + corridor.ToString ());
+		switch (corridor) {
+		case Sound4Corridor.Gang1:
+			clipToPlay = myList[0];
+			break;
+		case Sound4Corridor.Gang2:
+			clipToPlay = myList[1];
+			break;
+		case Sound4Corridor.Gang3:
+			clipToPlay = myList[2];
+			break;
+		case Sound4Corridor.Gang4:
+			clipToPlay = myList[3];
+			break;
+		case Sound4Corridor.Gang5:
+			clipToPlay = myList[4];
+			break;
+		default:
+			break;
+		}
+		if (clipToPlay == null) {
+			print ("Soundfile not found" + corridor.ToString());
+			return;
+		}
+		if (audio.isPlaying)
+			audio.Stop();
+
+		audio.loop = true;
+		audio.clip = clipToPlay;
+		audio.PlayOneShot (clipToPlay, 1.0F);
+		//audio.Play();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
